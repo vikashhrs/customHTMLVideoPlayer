63507c428dc083d4e443cd694f5031cab6999786
Vikash Kumar Sharma <vikashhrs@gmail.com>
2017-01-22 02:10:24 +0530

    commit

--- js/video-app.js
+++ js/video-app.js
@@ -2,3 +2,11 @@
  * Created by vikash on 21-Jan-17.
  */
 var app = angular.module('VideoApp',[]);
+
+app.controller('VideoController',['$scope','$window',function ($scope,$window) {
+    $scope.message = "Hello, AngularJs";
+    $scope.videoDisplay = document.getElementById("VideoElement");
+    $scope.videoSource = $window.videoSource;
+    $scope.titleDisplay = $window.titleDisplay;
+    $scope.videoDescription = $window.videoDescription;
+}])
